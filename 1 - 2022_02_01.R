73d7c403f8fe04633b4d23d6ff2e7faa8e0c2377
Laura Taylor <ltaylor18@elon.edu>
2022-02-01 16:12:33 -0500

    Day 1 - Notes 1 Intro to R/RStudio

---
+++ 1 - 2022_02_01.R
@@ -0,0 +1,48 @@
+#Day 1 - Tuesday, February 1, 2022
+7+3
+
+plot(1:10,seq(11,20))
+plot(1:5,1:5,main="Hello")
+
+log(100)
+log10(100)
+5+7
+sqrt(16)
+sqrt(-4)
+2^4
+2**4
+x <- 5 #Assign the value of 5 to x
+x
+x <- 1:5
+x
+x <- c(2,4,6,8)
+x
+x <- seq(1,10)
+x
+x <- seq(1,10,2)
+x
+mean(x)
+sd(x)
+var(x)
+grades <- c(80,90,88,73,96,85)
+grades #typo on purpose!
+summary(grades)
+max(grades)
+
+x = 5
+x
+
+demo(graphics)
+
+
+x <- 1:5
+y <- 1:5
+lm(y~x)
+mean(x)
+sd(x)
+var(x)
+
+?seq
+?rnorm
+
+rm(list=ls())
